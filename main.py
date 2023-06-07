e057cdf302b9860a8bf4a957232f30d0a52d5942
Acácio <99909819+acaciobjr@users.noreply.github.com>
2023-06-07 11:09:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,23 @@ from pdf2image import convert_from_path
 import difflib
 
 #caminhos
-caminho1 = r"C:\Users\Rose\Documents\listComp\Diretorio1"
-caminho2 = r"C:\Users\Rose\Documents\listComp\Diretorio2"
+caminho1 = r"Diretório 1 com a imagem original de texto"
+caminho2 = r"Diretório 2 com a imagem de menos texto que a imagem original"
 caminho_tesseract = r"C:\Program Files\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = os.path.join(caminho_tesseract, "tesseract.exe")
 
-def processar_texto(texto, caminho_arquivo):
-
-    if texto:
+def processar_texto(texto_limpo, caminho_arquivo):
+    if texto_limpo:
         print("Texto no Diretório 1 encontrado.")
     else:
         avisoFalha = f"Não foi possível identificar texto no arquivo {caminho_arquivo}."
         print(avisoFalha)
 
 def process_files(caminho):
+    texto = ''
     with open('texto do Diretório1.txt', 'w', encoding='utf-8') as f:
         for nome_arquivo in os.listdir(caminho):
             caminho_arquivo = os.path.join(caminho, nome_arquivo)
-            texto = ''
             if nome_arquivo.endswith('.pdf'):
                 try:
                     imagens = convert_from_path(caminho_arquivo)
@@ -40,31 +39,29 @@ def process_files(caminho):
                     img = cv2.imread(caminho_arquivo)
                     if img is not None:
                         texto = pytesseract.image_to_string(img)
-                        f.write(texto + "\n")
+                        texto_limpo = texto.replace('  ', '').replace('   ', '').replace('-', '').replace('_', '').replace('—', '').replace('.', '').replace('TECNICO ENFERMAGEM', '').replace('HOSPITAL JACAREPAGUA', '')
+                        f.write(texto_limpo + "\n")
+                        processar_texto(texto_limpo, caminho_arquivo)
                 except Exception as e:
                     print(f"Erro ao processar a imagem {caminho_arquivo}: {str(e)}")
 
-            processar_texto(texto, caminho_arquivo)
 
 process_files(caminho1)
-
-
-def processar_texto2(texto2, caminho_arquivo2):
-
-    if texto2:
+def processar_texto2(texto_limpo2, caminho_arquivo):
+    if texto_limpo2:
         print("Texto no Diretório 2 encontrado.")
     else:
         avisoFalha = f"Não foi possível identificar texto no arquivo {caminho_arquivo}."
         print(avisoFalha)
 
 def process_files2(caminho):
+    texto2 = ''
     with open('texto do Diretório2.txt', 'w', encoding='utf-8') as f:
         for nome_arquivo in os.listdir(caminho):
-            caminho_arquivo2 = os.path.join(caminho, nome_arquivo)
-            texto2 = ''
+            caminho_arquivo = os.path.join(caminho, nome_arquivo)
             if nome_arquivo.endswith('.pdf'):
                 try:
-                    imagens = convert_from_path(caminho_arquivo2)
+                    imagens = convert_from_path(caminho_arquivo)
                     for imagem in imagens:
                         temp_img_path = 'temp_img.jpg'
                         imagem.save(temp_img_path, 'JPEG')
@@ -73,31 +70,32 @@ def process_files2(caminho):
                             texto2 += pytesseract.image_to_string(img)
                         os.remove(temp_img_path)
                 except Exception as e:
-                    print(f"Erro ao processar o arquivo PDF {caminho_arquivo2}: {str(e)}")
+                    print(f"Erro ao processar o arquivo PDF {caminho_arquivo}: {str(e)}")
             elif nome_arquivo.endswith('.jpeg'):
                 try:
-                    img = cv2.imread(caminho_arquivo2)
+                    img = cv2.imread(caminho_arquivo)
                     if img is not None:
                         texto2 = pytesseract.image_to_string(img)
-                        f.write(texto2 + "\n")
+                        texto_limpo2 = texto2.replace('  ', '').replace('   ', '').replace('-', '').replace('_', '').replace('—', '').replace('.', '').replace('TECNICO ENFERMAGEM', '').replace('HOSPITAL JACAREPAGUA', '')
+                        f.write(texto_limpo2 + "\n")
+                        processar_texto2(texto_limpo2, caminho_arquivo)
                 except Exception as e:
-                    print(f"Erro ao processar a imagem {caminho_arquivo2}: {str(e)}")
-
-            processar_texto2(texto2, caminho_arquivo2)
+                    print(f"Erro ao processar a imagem {caminho_arquivo}: {str(e)}")
 
 process_files2(caminho2)
 
 
-#def separando(texto, texto2):
- #   with open('texto.txt', 'w', encoding='utf-8') as f:
- #       diferenca = difflib.ndiff(texto1.splitlines(), texto2.splitlines())
- #       linhas_diferentes = [linha for linha in diferenca if linha.startswith('- ') or linha.startswith('+ ')]
- #       return '\n'.join(linhas_diferentes)
+def separando(caminho_texto1, caminho_texto2):
+    with open('diferenca.txt', 'w', encoding='utf-8') as f:
+        with open(caminho_texto1, 'r', encoding='utf-8') as arquivo1:
+            texto1 = arquivo1.readlines()
+        with open(caminho_texto2, 'r', encoding='utf-8') as arquivo2:
+            texto2 = arquivo2.readlines()
 
-    # Exemplo de uso
-    #texto = "Este é o primeiro texto.\nEle tem algumas diferenças."
-    #texto2 = "Este é o primeiro texto.\nEle tem algumas diferenças.\n2"
+        nomes1 = set(texto1)
+        nomes2 = set(texto2)
 
-    #diferencas = separando(texto, texto2)
-    #f.write(diferencas + "\n")
-    #print(diferencas)
+        nomes_ausentes = nomes1.difference(nomes2)
+
+        f.write('\n'.join(nomes_ausentes) + "\n")
+separando('texto do Diretório1.txt', 'texto do Diretório2.txt')
